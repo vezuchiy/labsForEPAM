8bfd7f89516313619029f3a84a2f6556b78c4b5c
vezuchiy <truehackerman@gmail.com>
2020-05-28 20:32:36 +0300

    simple restful service

---
+++ src/test/java/com/app/services/ConverterServiceTest.java
@@ -0,0 +1,21 @@
+package com.app.services;
+
+import com.app.models.UserRequest;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConverterServiceTest {
+    @Test
+    void processCheckTesting() {
+        UserRequest request = new UserRequest((double) 12, "inches", "meters");
+        ConverterService service = new ConverterService();
+
+        assertEquals(service.processCheck(request), 1);
+        request.setDestination("kilometers");
+        assertEquals(service.processCheck(request), -1);
+        request.setDestination("meters");
+        request.setNumber((double) -10);
+        assertEquals(service.processCheck(request), -2);
+    }
+}
